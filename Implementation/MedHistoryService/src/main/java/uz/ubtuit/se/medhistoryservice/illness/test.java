664ed3414d50652602a4d7deac6d52f7041ce3d9
DK <kuryazov@se.uni-oldenburg.de>
2019-07-30 15:29:03 +0500

    Illness with error

---
+++ Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/illness/test.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uz.ubtuit.se.medhistoryservice.illness;
+
+/**
+ *
+ * @author dilshod
+ */
+public class test {
+    public void sayHallo(){
+        System.out.println("Salom Dunyo");
+        eshmat;
+    }
+    
+}
