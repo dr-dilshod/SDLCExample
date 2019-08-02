371c501a2297aabdaf0ba405c8e3e02066a440a8
sherxonov <MERCEDES@DESKTOP-G3TLQQK>
2019-08-02 14:21:12 +0500

    some commit

---
+++ Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/history/HistoryController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uz.ubtuit.se.medhistoryservice.history;
+
+/**
+ *
+ * @author MERCEDES
+ */
+public class HistoryController {
+    System.out.println("Test this");
+}
