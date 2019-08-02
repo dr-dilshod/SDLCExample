d07d2f28e83a4000cff62cd1de7c9fc8528a24cf
sherxonov <MERCEDES@DESKTOP-G3TLQQK>
2019-08-02 14:23:37 +0500

    history model and controller finished

--- Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/history/History.java
+++ Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/history/History.java
@@ -5,10 +5,69 @@
  */
 package uz.ubtuit.se.medhistoryservice.history;
 
+import javax.ws.rs.Path;
+
 /**
  *
- * @author dilshod
+ * @author xursand
  */
+@Path(value = "history")
 public class History {
+    private String name;
+    private String symptom;
+    private String reason;
+    private String severity;
+    private String patient;
+    private String doctor;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSymptom() {
+        return symptom;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public String getSeverity() {
+        return severity;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public String getDoctor() {
+        return doctor;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSymptom(String symptom) {
+        this.symptom = symptom;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setSeverity(String severity) {
+        this.severity = severity;
+    }
+
+    public void setPatient(String patient) {
+        this.patient = patient;
+    }
+
+    public void setDoctor(String doctor) {
+        this.doctor = doctor;
+    }
+
+    
     
 }
