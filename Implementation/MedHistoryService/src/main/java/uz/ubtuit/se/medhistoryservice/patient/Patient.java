584a1435fbe1434380eed8481f98943f8dbb75cb
Bekturdi_ <Bekturdi_@DESKTOP-IBV7IBI>
2019-08-05 17:17:36 +0500

    java class added patient java

--- Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/patient/Patient.java
+++ Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/patient/Patient.java
@@ -1,14 +1,67 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+ 
 package uz.ubtuit.se.medhistoryservice.patient;
+
+import java.util.Date;
+//import sun.util.calendar.LocalGregorianCalendar;
+//
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.ArrayList;
+//import java.util.List;
+//import javax.ws.rs.Path;
+//import javax.ws.rs.core.MediaType;
+//import uz.ubtuit.se.medhistoryservice.config.DBConnection;
+
 
 /**
  *
- * @author dilshod
+ * @author bekturdi
  */
 public class Patient {
+
+    public String name;
+    public String surname;
+    public String login;
+    public String password;
+    public Date birthDate;
+    public String address;
+    public String phone;
+    public String blood_group;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setBlood_group(String blood_group) {
+        this.blood_group = blood_group;
+    }
     
+    
+
 }
