43a8dc05cbded7d2053bb8ba661e0ea908291a3e
Bekturdi_ <Bekturdi_@DESKTOP-IBV7IBI>
2019-08-05 17:49:27 +0500

    patient controller edit java class

--- Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/patient/PatientController.java
+++ Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/patient/PatientController.java
@@ -11,10 +11,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-//import javax.ws.rs.GET;
-//import javax.ws.rs.Path;
-//import javax.ws.rs.Produces;
-//import javax.ws.rs.core.MediaType;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import uz.ubtuit.se.medhistoryservice.config.DBConnection;
 
 
