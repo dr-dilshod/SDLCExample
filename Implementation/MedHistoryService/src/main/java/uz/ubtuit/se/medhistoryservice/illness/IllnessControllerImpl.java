e53aa9ee0cdaacd9107eb23cae53d932d8d6708c
Delux <Delux@DESKTOP-OB05QH0>
2019-08-06 14:45:41 +0500

    Illness

--- Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/illness/IllnessControllerImpl.java
+++ Implementation/MedHistoryService/src/main/java/uz/ubtuit/se/medhistoryservice/illness/IllnessControllerImpl.java
@@ -1,6 +1,7 @@
 
 package uz.ubtuit.se.medhistoryservice.illness;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -18,9 +19,9 @@ import uz.ubtuit.se.medhistoryservice.illness.Illness;
  */
 public class IllnessControllerImpl {
     @GET
-    @Path("/GetAllillness")
+    @Path("/GetAllillneses")
     @Produces("application/json")
-    public List<Illness> getillness() {
+    public List<Illness> getillneses() {
         DBConnection mysqlConnect = new DBConnection();
         String sql = "SELECT * FROM illness";
         List<Illness> illneses = new ArrayList<>();
@@ -51,7 +52,7 @@ public class IllnessControllerImpl {
     @GET
     @Path("/GetSymptomByID/{id}")
     @Produces("application/json")
-    public Illness getIllnesByID(int id) {
+    public Illness getIllnessByID(int id) {
         DBConnection mysqlConnect = new DBConnection();
         String sql = "SELECT * FROM illness WHERE ID=" + id;
         Illness ill = new Illness();
@@ -73,7 +74,7 @@ public class IllnessControllerImpl {
     public void addSymptom(Illness illness) {
         DBConnection dbc = new DBConnection();
         try { 
-            String sql = "INSERT INTO illness(Name, reason, serverity, symptom) "
+            String sql = "INSERT INTO illness(Name, reason, severity, symptom) "
                     + "VALUES (?,?,?,?)";
             java.sql.PreparedStatement preparedStatement = dbc.connect().prepareStatement(sql);
             preparedStatement.setString(1, illness.getName());
@@ -89,7 +90,78 @@ public class IllnessControllerImpl {
         } 
   
     }
+    public void addIllness(Illness illness) {
+        DBConnection dbc = new DBConnection();
+        try { 
+            String sql = "INSERT INTO illness(Name, reason, severity, symptom)"
+                    + " VALUES (?,?,?,?)";
+            java.sql.PreparedStatement preparedStatement = dbc.connect().prepareStatement(sql);
+            preparedStatement.setString(1, illness.getName());
+            preparedStatement.setString(2, illness.getReason());
+            preparedStatement.setString(3, illness.getSeverity());
+            preparedStatement.setString(4, illness.getSymptom());
+            
+          preparedStatement.executeUpdate(); 
+            dbc.disconnect();
+        } catch (Exception e) {  
+            
+            System.err.println("Got an exception! "); 
+            System.err.println(e.getMessage()); 
+        } 
+    }
     
     
-   
+     public void deleteIllness(int id) {
+        DBConnection dbc = new DBConnection();
+        
+        try
+        {
+          String query = "DELETE FROM illness WHERE id=?";
+          PreparedStatement preparedStmt = dbc.connect().prepareStatement(query);
+          preparedStmt.setInt(1, id);
+          preparedStmt.execute();
+          dbc.disconnect();
+        }
+        catch (Exception e)
+        {
+          System.err.println("Got an exception! ");
+          System.err.println(e.getMessage());
+        }
+    }
+    
+   public void updateIllness(Illness illness) {
+    DBConnection dbc = new DBConnection();
+    
+    try
+    {
+      // create the java mysql update preparedstatement
+      String query = "UPDATE illness SET Name=?, reason=?, severity=?, symptom=?"
+              + " WHERE Id=?";
+      PreparedStatement preparedStatement = dbc.connect().prepareStatement(query);
+        preparedStatement.setString(1, illness.getName());
+        preparedStatement.setString(2, illness.getReason());
+        preparedStatement.setString(2, illness.getSeverity());
+        preparedStatement.setString(2, illness.getSymptom());
+        preparedStatement.setInt(3, illness.getId());
+
+      // execute the java preparedstatement
+      preparedStatement.executeUpdate();
+      
+      dbc.disconnect();
+    }
+    catch (Exception e)
+    
+    {
+      System.err.println("Got an exception! ");
+      System.err.println(e.getMessage());
+    }
+    }
+
+    public List<Illness> getAllIllneses() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+     
+     
 }
