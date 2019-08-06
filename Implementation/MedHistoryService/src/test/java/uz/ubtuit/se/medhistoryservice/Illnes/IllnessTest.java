7cd164be00ef91dd1e223b6d40c9bfecf2532416
Delux <Delux@DESKTOP-OB05QH0>
2019-08-06 11:26:45 +0500

    Illness test

---
+++ Implementation/MedHistoryService/src/test/java/uz/ubtuit/se/medhistoryservice/Illnes/IllnessTest.java
@@ -0,0 +1,74 @@
+
+package uz.ubtuit.se.medhistoryservice.Illnes;
+
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import uz.ubtuit.se.medhistoryservice.illness.Illness;
+import uz.ubtuit.se.medhistoryservice.illness.IllnessControllerImpl;
+
+/**
+ *
+ * @author Egamberdi Otoxonov
+ */
+public class IllnessTest {
+    
+    public IllnessTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void deleteIllness(){
+    int id = 1;
+    IllnessControllerImpl i = new IllnessControllerImpl(); 
+    i.deleteIllness(id);
+    }
+    
+     @Test
+    public void testAddIllness(){
+        Illness i = new Illness();
+        i.setName("uxlash");
+        i.setReason("kop uxlash");
+        i.setSeverity("o'rtacha");
+        i.setSymptom("dagasalik");
+        IllnessControllerImpl ill = new IllnessControllerImpl();
+        ill.addIllness(i);
+    }
+    
+     @Test
+    public void updateIllness(){
+    Illness i = new Illness();
+    i.setName("gripp");
+    i.setReason("shamollash"); 
+    }
+    
+     @Test 
+    public void testGetAllIllneses() {
+        IllnessControllerImpl ill = new IllnessControllerImpl();
+        List<Illness> all = ill.getAllIllneses();
+        for (Illness i : all) {
+            System.out.println("name = " + i.getName()+ "| reason = " + i.getReason() + "| severity = " i.getSeverity() + "| symptom = " + i.getSymptom());
+        }
+    }
+    
+    
+}
