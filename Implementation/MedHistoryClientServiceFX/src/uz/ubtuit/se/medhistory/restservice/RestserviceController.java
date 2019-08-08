8bc9b6fca73a1e852cc66760d60f29211de80069
sherxonov <MERCEDES@DESKTOP-G3TLQQK>
2019-08-08 16:26:46 +0500

    new all package javafx added

---
+++ Implementation/MedHistoryClientServiceFX/src/uz/ubtuit/se/medhistory/restservice/RestserviceController.java
@@ -0,0 +1,16 @@
+package uz.ubtuit.se.medhistory.restservice;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by MERCEDES on 8/8/2019.
+ */
+public class RestserviceController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
